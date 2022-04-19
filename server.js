90120cb0d9c02d67695bd92e1c3c4182c6562af3
teynman <t.wenyuanhuang@gmail.com>
2022-04-19 16:54:35 -0400

    Connection to MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const cors = require('cors');
 const app = express();
+mongoose.connect('mongodb+srv://yelpproject:codingisfun@cluster0.cf2bs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+app.use(express.json());
+
 app.get('/hello', (req, res) => {res.send('Hello World!')})
 app.listen(4000);
