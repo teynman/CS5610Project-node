0169cd8006333ab006a5a51183e8094a64fd740e
stanat23 <wang.zun@northeastern.edu>
2022-04-21 01:36:44 -0400

    updated user-checkins-model typo

--- database/user-checkins/user-checkins-model.js
+++ database/user-checkins/user-checkins-model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const userCheckinsSchema = require('./user-checkins-schema');
 
 const userCheckinsModel = mongoose.model(
-    'UserReviewsModel', userCheckinsSchema
+    'UserCheckinsModel', userCheckinsSchema
 )
 
 module.exports = userCheckinsModel;
