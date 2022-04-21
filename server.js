f36c5152d572e5e67e3934373c98032dff50dece
stanat23 <wang.zun@northeastern.edu>
2022-04-21 01:28:24 -0400

    updated controllers in server.js

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const session = require('express-session');
 const cors = require('cors');
 const app = express();
 mongoose.connect('mongodb+srv://yelpproject:codingisfun@cluster0.cf2bs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+app.use(cors());
 app.use(express.json());
+
+require("./controllers/users-controller")(app);
+require("./controllers/bookmarks-controller")(app);
+require("./controllers/user-following-controller")(app);
+require("./controllers/auth-controller")(app);
+require("./controllers/user-reviews-controller")(app);
+require("./controllers/user-checkins-controller")(app);
 
 app.get('/hello', (req, res) => {res.send('Hello World!')})
 app.listen(process.env.PORT || 4000);
