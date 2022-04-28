ba48c4d6ea9decd32a39275d799ea96acceced3f
stanat23 <wang.zun@northeastern.edu>
2022-04-28 05:10:12 -0400

    updated server

--- database/user-bookmarks/user-bookmarks-dao.js
+++ database/user-bookmarks/user-bookmarks-dao.js
@@ -17,7 +17,7 @@ const createUserBookmarks = (userBookmark) => {
 }
 
 const addUserBookmark = (uid, bid) => {
-    return userBookmarksModel.update({_id: uid}, {
+    return userBookmarksModel.findOneAndUpdate({_id: uid}, {
         $addToSet: {
             bookmarks: bid
         },
