ecad632b3c8572d36f886b0d930a4af9c10c5e52
stanat23 <wang.zun@northeastern.edu>
2022-04-28 02:44:01 -0400

    updated server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(session({
     cookie: { secure: true, sameSite: 'None' }
 }));
 app.use(express.json());
-// 1
+// 2
 require("./controllers/users-controller")(app);
 require("./controllers/bookmarks-controller")(app);
 require("./controllers/user-following-controller")(app);
